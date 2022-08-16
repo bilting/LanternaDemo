dfc37ab05a461318898cc0f9bca36809f749657a
Ulf Bilting <ulf@bilting.se>
2022-08-16 11:09:47 +0200

    Original files

---
+++ src/main/java/LanternaLab.java
@@ -0,0 +1,35 @@
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.awt.*;
+import java.io.IOException;
+
+public class LanternaLab {
+    final static TextColor RED = new TextColor.RGB(255,0,0);
+    final static TextColor GREEN = new TextColor.RGB(0,255,0);
+    static Terminal terminal;
+    public static void main(String[] args) throws IOException {
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
+        terminal = terminalFactory.createTerminal();
+        terminal.setCursorVisible(false);
+        terminal.setForegroundColor(new TextColor.RGB(255,0,0));
+        terminal.setBackgroundColor(new TextColor.RGB(255,255,255));
+        for (int row = 1; row <= 24; row++) {
+            for (int col = 1; col <= 80; col++) {
+                out(row,col,'2', GREEN);
+            }
+        }
+        out(4,4,'X', RED);
+
+
+
+    }
+    static void out(int row, int col, char c, TextColor color) throws IOException {
+        terminal.setForegroundColor(color);
+        terminal.setCursorPosition(col, row);
+        terminal.putCharacter(c);
+        terminal.flush();
+    }
+
+}
